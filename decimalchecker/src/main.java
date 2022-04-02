6696a7260a159b4bc808f61380bc175196017bbf
deepakniranjan <102375754+deepakniranjan@users.noreply.github.com>
2022-04-02 11:01:19 -0700

    Add files via upload

---
+++ decimalchecker/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[]arg){
+
+    }
+    public static void decimal(double a,double b){
+        if(a==b){
+            System.out.println("it is same")
+        }
+    }
+}
